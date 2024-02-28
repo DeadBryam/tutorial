e58953bd4799557524b9b0265e51774d6981c4ac
DeadBryam <Bryamu.u@gmail.com>
2024-02-28 11:45:27 -0600

    refactor: add onchange, onend, and oninit events

--- lib/src/controller/tutorial_tutorial.dart
+++ lib/src/controller/tutorial_tutorial.dart
@@ -6,80 +6,90 @@ import 'package:tutorial/src/painter/painter.dart';
 
 class Tutorial {
   //FUNÇÃO QUE EXIBE O TUTORIAL
-  static showTutorial(BuildContext context, List<TutorialItem> children) async {
+  static showTutorial(
+    BuildContext context,
+    List<TutorialItem> children, {
+    void Function()? onInit,
+    void Function(int index)? onChange,
+    void Function()? onEnd,
+  }) async {
     int count = 0;
     var size = MediaQuery.of(context).size;
     OverlayState? overlayState = Overlay.of(context);
     List<OverlayEntry> entrys = [];
-    children.forEach((element) async {
-      var offset = _capturePositionWidget(element.globalKey);
-      var sizeWidget = _getSizeWidget(element.globalKey);
-      entrys.add(
-        OverlayEntry(
-          builder: (context) {
-            return GestureDetector(
-              onTap: element.touchScreen == true
-                  ? () {
-                      entrys[count].remove();
-                      count++;
-                      if (count != entrys.length) {
-                        overlayState?.insert(entrys[count]);
-                      }
-                    }
-                  : () {},
-              child: Scaffold(
-                backgroundColor: Colors.transparent,
-                body: Stack(
-                  children: [
-                    CustomPaint(
-                      size: size,
-                      painter: HolePainter(
+
+    void _nextTutorial() {
+      entrys[count].remove();
+      count++;
+      if (count != entrys.length) {
+        onChange?.call(count);
+        overlayState.insert(entrys[count]);
+      }
+
+      if (count == entrys.length) {
+        onEnd?.call();
+      }
+    }
+
+    children.forEach(
+      (element) async {
+        var offset = _capturePositionWidget(element.globalKey);
+        var sizeWidget = _getSizeWidget(element.globalKey);
+        entrys.add(
+          OverlayEntry(
+            builder: (context) {
+              return GestureDetector(
+                onTap: element.touchScreen == true ? _nextTutorial : () {},
+                child: Scaffold(
+                  backgroundColor: Colors.transparent,
+                  body: Stack(
+                    children: [
+                      CustomPaint(
+                        size: size,
+                        painter: HolePainter(
                           shapeFocus: element.shapeFocus,
                           dx: offset.dx + (sizeWidget.width / 2),
                           dy: offset.dy + (sizeWidget.height / 2),
                           width: sizeWidget.width,
-                          height: sizeWidget.height),
-                    ),
-                    Positioned(
-                      top: element.top,
-                      bottom: element.bottom,
-                      left: element.left,
-                      right: element.right,
-                      child: Container(
-                        width: size.width * 0.8,
-                        child: Column(
-                          crossAxisAlignment: element.crossAxisAlignment,
-                          mainAxisAlignment: element.mainAxisAlignment,
-                          children: [
-                            ...element.children!,
-                            GestureDetector(
-                              child: element.widgetNext ??
-                                  Text(
-                                    "NEXT",
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                              onTap: () {
-                                entrys[count].remove();
-                                count++;
-                                if (count != entrys.length) {
-                                  overlayState?.insert(entrys[count]);
-                                }
-                              },
-                            ),
-                          ],
+                          height: sizeWidget.height,
                         ),
                       ),
-                    )
-                  ],
+                      Positioned(
+                        top: element.top,
+                        bottom: element.bottom,
+                        left: element.left,
+                        right: element.right,
+                        child: Container(
+                          width: size.width * 0.8,
+                          child: Column(
+                            crossAxisAlignment: element.crossAxisAlignment,
+                            mainAxisAlignment: element.mainAxisAlignment,
+                            children: [
+                              ...element.children!,
+                              GestureDetector(
+                                child: element.widgetNext ??
+                                    Text(
+                                      "NEXT",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                onTap: _nextTutorial,
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
-        ),
-      );
-    });
+              );
+            },
+          ),
+        );
+      },
+    );
 
-    overlayState?.insert(entrys[0]);
+    overlayState.insert(entrys[0]);
+    onInit?.call();
   }
 
   //FUNÇÃO PARA CAPTURAR A POSIÇÃO DO COMPONENTE
